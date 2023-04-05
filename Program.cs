83352dc39bf97759f452593e116a25b923e39070
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2023-04-05 15:15:34 -0500

    work on lesson 7 on type conversion

--- Program.cs
+++ Program.cs
@@ -1,3 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program {
 
+    static void Main() {
+
+        //Implicit casting
+        ////sbyte
+        ////sbyte a = 10;
+
+        ////char
+        //char a = 'A';
+
+        ////int
+        //int b;
+
+        ////copy the value from a to b using implicit casting
+        //b = a;
+
+        //Console.WriteLine(a);
+        //Console.WriteLine(b);
+        //Console.ReadKey();
+        //--------------------------------------------------------------
+        //Explicit Casting
+        //int
+        int a = 500;
+
+        //byte
+        byte b;
+
+        //copy the value of a to b with explicit casting
+        //int to byte loosy conversion a = 500, b = 244;
+        b = (byte)a;
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.ReadKey();
+        //------------------------------------------------------------------
+        //Parsing
+
+
+    }
+
+}
